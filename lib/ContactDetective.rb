4f7888ad9cb686a7099ad4e565423562741c4b64
Joshua Weinstein <jweinst1@berkeley.edu>
2016-01-07 20:39:24 -0800

    more functions

--- lib/ContactDetective.rb
+++ lib/ContactDetective.rb
@@ -1,6 +1,11 @@
 require "ContactDetective/version"
 
 module ContactDetective
+  #gets html from a link
+  def gethtmlfromlink(link)
+    require 'open-uri'
+    return open(link, &:read)
+  end
   class Emails
 
   end
@@ -9,5 +14,13 @@ module ContactDetective
   end
   class Addresses
 
+  end
+  class Utils
+    def removebrackets(text)
+      text.split(/<[^<>]+>/)
+    end
+    def onlybrackets(text)
+      text.scan(/<[^<>]+>/)
+    end
   end
 end
