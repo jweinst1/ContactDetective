58b089c8836be0d3f95a92b31b272ff6dcc66c6f
Joshua Weinstein <jweinst1@berkeley.edu>
2016-01-07 23:13:47 -0800

    more utils

--- lib/ContactDetective.rb
+++ lib/ContactDetective.rb
@@ -16,11 +16,29 @@ module ContactDetective
 
   end
   class Utils
+
     def removebrackets(text)
       text.split(/<[^<>]+>/)
     end
     def onlybrackets(text)
       text.scan(/<[^<>]+>/)
+    end
+    def replacebrackets(text)
+      text.gsub(/<[^<>]+>/, "")
+    end
+    def sentfragments(text)
+      wordfrags = []
+      text.split(/<[^<>]+>/).each do |elem|
+        if elem =~ /^[a-zA-Z]+ [a-zA-Z]*$/
+          wordfrags << elem
+        end
+      end
+      return wordfrags
+    end
+    def getwords(text)
+      words = text.scan(/ ([a-zA-Z]+) /)
+      filtered = []
+      words.each {|elem| filtered << elem[0]}
     end
   end
 end
